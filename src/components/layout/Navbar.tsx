d4019e9ce81ea6d4a0da23280230fe7b637a1b28
nicmackenzie <nicmackenzie@live.com>
2023-09-19 15:23:30 +0300

    content wrapper height and padding

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,7 +4,7 @@ import UserMenu from './UserMenu';
 
 export default function Navbar() {
   return (
-    <div className="flex items-center p-4 bg-gray-100 dark:bg-slate-900 border-b h-16">
+    <div className="flex items-center p-4 bg-gray-100 dark:bg-slate-900 border-b h-20">
       <Button variant="outline" size="icon" className="md:hidden">
         <MenuIcon />
       </Button>
