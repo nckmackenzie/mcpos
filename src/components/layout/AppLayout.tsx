a1c027b1756b2b6446b97d4e9c499455b672f5e2
nicmackenzie <nicmackenzie@live.com>
2023-09-19 15:24:27 +0300

    define content wrapper

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -6,9 +6,11 @@ export default function AppLayout() {
   return (
     <div className="h-dvh relative">
       <Aside />
-      <main className="md:pl-72">
+      <main className="md:pl-72 h-full overflow-scroll">
         <Navbar />
-        <Outlet />
+        <div className="h-[calc(100dvh-5rem)] p-4 md:p-6 lg:px-8 lg:py-6">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
