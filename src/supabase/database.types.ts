c2e8cc8ae016fe32128ab5b9575ab7771a0996a6
nicmackenzie <nicmackenzie@live.com>
2023-09-19 21:40:09 +0300

    redefine schema types

--- src/supabase/database.types.ts
+++ src/supabase/database.types.ts
@@ -4,38 +4,77 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
+      companies: {
+        Row: {
+          address: string | null
+          city: string | null
+          companyName: string
+          contact: string
+          contactPerson: string
+          country: string
+          createdAt: string | null
+          email: string
+          id: string
+          postalCode: string | null
+        }
+        Insert: {
+          address?: string | null
+          city?: string | null
+          companyName?: string
+          contact?: string
+          contactPerson?: string
+          country?: string
+          createdAt?: string | null
+          email?: string
+          id?: string
+          postalCode?: string | null
+        }
+        Update: {
+          address?: string | null
+          city?: string | null
+          companyName?: string
+          contact?: string
+          contactPerson?: string
+          country?: string
+          createdAt?: string | null
+          email?: string
+          id?: string
+          postalCode?: string | null
+        }
+        Relationships: []
+      }
       currencies: {
         Row: {
-          code: string;
-          name: string | null;
-        };
+          code: string
+          name: string | null
+        }
         Insert: {
-          code?: string;
-          name?: string | null;
-        };
+          code?: string
+          name?: string | null
+        }
         Update: {
-          code?: string;
-          name?: string | null;
-        };
-        Relationships: [];
-      };
-    };
+          code?: string
+          name?: string | null
+        }
+        Relationships: []
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
