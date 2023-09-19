0f07bfeaf32b8d0edcf0fb9eb1ebcc8478b6d039
nicmackenzie <nicmackenzie@live.com>
2023-09-19 14:55:35 +0300

    change app theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -77,6 +77,9 @@ module.exports = {
       minHeight: {
         dvh: '100dvh',
       },
+      height: {
+        dvh: '100dvh',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
