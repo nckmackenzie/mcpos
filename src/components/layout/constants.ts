0550a8bc4fbaf23eb430cc1fd0f59099957e40a9
nicmackenzie <nicmackenzie@live.com>
2023-09-20 11:33:14 +0300

    Nav items

---
+++ src/components/layout/constants.ts
@@ -0,0 +1,65 @@
+import {
+  BoxesIcon,
+  CalculatorIcon,
+  ClipboardListIcon,
+  GaugeCircleIcon,
+  MessagesSquareIcon,
+  SettingsIcon,
+  ShoppingBagIcon,
+  ShoppingCartIcon,
+  User2Icon,
+  Users2Icon,
+} from 'lucide-react';
+
+export const NAVITEMS = [
+  {
+    label: 'Dashboard',
+    icon: GaugeCircleIcon,
+    to: '/',
+  },
+  {
+    label: 'Products',
+    icon: ShoppingBagIcon,
+    to: '/products',
+  },
+  {
+    label: 'Customers',
+    icon: Users2Icon,
+    to: '/customers',
+  },
+  {
+    label: 'Inventory',
+    icon: BoxesIcon,
+    to: '/inventory',
+  },
+  {
+    label: 'Orders',
+    icon: ShoppingCartIcon,
+    to: '/orders',
+  },
+  {
+    label: 'Vendors',
+    icon: User2Icon,
+    to: '/vendors',
+  },
+  {
+    label: 'Invoices',
+    icon: ClipboardListIcon,
+    to: '/invoices',
+  },
+  {
+    label: 'Expenses',
+    icon: CalculatorIcon,
+    to: '/expenses',
+  },
+  {
+    label: 'Messages',
+    icon: MessagesSquareIcon,
+    to: '/expenses',
+  },
+  {
+    label: 'Settings',
+    icon: SettingsIcon,
+    to: '/settings',
+  },
+];
