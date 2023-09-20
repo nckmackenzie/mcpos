78f0affc2d7f74f22f4bf9d2597e12e177f7e274
nicmackenzie <nicmackenzie@live.com>
2023-09-20 13:02:32 +0300

    Add routes and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,69 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Auth from './pages/Auth';
-import Dashboard from './pages/Dashboard';
-import Redirect from './pages/Redirect';
 import { Toaster } from './components/ui/toaster';
+
+import ProtectedRoute from './pages/ProtectedRoute';
+import AppLayout from './components/layout/AppLayout';
+import Loader from './components/ui/loader';
+
+const Auth = lazy(() => import('./pages/Auth'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const Redirect = lazy(() => import('./pages/Redirect'));
+const Company = lazy(() => import('./pages/Company'));
+const Products = lazy(() => import('./pages/Products'));
+const Inventory = lazy(() => import('./pages/Inventory'));
+const Settings = lazy(() => import('./pages/Settings'));
+const Customers = lazy(() => import('./pages/Customers'));
+const Orders = lazy(() => import('./pages/Orders'));
+const Vendors = lazy(() => import('./pages/Vendors'));
+const Invoices = lazy(() => import('./pages/Invoices'));
+const Expenses = lazy(() => import('./pages/Expenses'));
+const Messages = lazy(() => import('./pages/Messages'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/success-signup-redirect" element={<Redirect />} />
-          <Route path="/" element={<Dashboard />} />
+          <Route
+            path="/auth"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Auth />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/success-signup-redirect"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Redirect />
+              </Suspense>
+            }
+          />
+          <Route
+            element={
+              <Suspense fallback={<Loader />}>
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              </Suspense>
+            }
+          >
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/company" element={<Company />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/inventory" element={<Inventory />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/vendors" element={<Vendors />} />
+            <Route path="/invoices" element={<Invoices />} />
+            <Route path="/expenses" element={<Expenses />} />
+            <Route path="/messages" element={<Messages />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
       <Toaster />
